2c595c32514d4f96fdd0fe9e62297e9dab63d6b8
zeinz <zeinz@192.168.2.10>
2019-07-21 18:20:23 -0400

    My first commit#1 Uploading files My Second Commit ( Uploading Diagram) #2 Ian, third commit ( creating card Abstract class) #3 Ian, fourth commit ( re designed class diagram)#4 Ian , Fifth commit #5

--- src/ca/sheridancollege/project/WarGame.java
+++ src/ca/sheridancollege/project/WarGame.java
@@ -1,25 +1,41 @@
-
 package ca.sheridancollege.project;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author Ian Ibrahim @ Zeinith.com
  */
-public class WarGame extends Game{
+public class WarGame extends Game {
 
     private int numOfPlayers;
-    private ArrayList < Player> players;
+    private Player[] players;
+    private PileOfCards gameDeck = new PileOfCards(52);
+
     public WarGame(String givenName) {
-        super(givenName);
-        
-        
+        super("War Game");
+
     }
 
     @Override
     public void play() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        gameDeck.fill();
+        System.out.println(gameDeck.showCards());
+        gameDeck.shuffle();
+        System.out.println(gameDeck.showCards());
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please Select the number of players ");
+        Player player1 = new PCplayer("Alfa");
+        Player player2 = new HumanPlayer ("mont");
+        player1.play();
+        player2.play();
+
+    }
+    
+    public void truce() { 
+        
     }
 
     @Override
